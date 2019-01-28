db409696dc03ff18d0bb7fa18aeee98f3ba8526a
Valerija Krastina <valerijakrastina@Valerijas-MacBook-Air.local>
2019-01-28 22:46:53 +0100

    Heroku setup

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js https://medium.com/netscape/deploying-a-vue-js-2-x-app-to-heroku-in-5-steps-tutorial-a69845ace489
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
